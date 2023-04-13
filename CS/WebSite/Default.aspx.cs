532656e179ab4fb7dd924353f9e8260cb1ed83c0
Svetlana Nikulina <svetlana.nikulina@devexpress.com>
2023-04-13 11:12:22 +0400

    update readme (#1)

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -8,10 +8,8 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 
-public partial class _Default : System.Web.UI.Page 
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
 
     }
 }
